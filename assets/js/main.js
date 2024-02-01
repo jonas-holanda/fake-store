aa3fbef27fed75da9d6b05035fe243788b0bf933
Jonas Holanda <jonasholanda15@gmail.com>
2024-01-31 23:10:09 -0300

    feat: fake-store finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+document.querySelector('#date').innerHTML = (new Date()).getFullYear();
+
+const getProducts = async () => {
+    const url = `https://fakestoreapi.com/products`;   
+    const response = await fetch(url);
+
+    if (response.status === 200) {
+        const data = await response.json();
+        renderCard(data);
+    } else {
+        renderCard(false);
+    }
+}
+
+const renderCard = (data) => {
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+    if (!data) {
+        container.innerHTML = `<h2 style='color:red;text-align:center'>Erro ao carregar os Produtos. Tente Novamente.</h2>`;
+    } else {
+
+        for (let i=0; i<data.length; i++) {
+            const {image,description,title,price,category,rating} = data[i];
+
+            const containerCard = document.createElement('section');
+            containerCard.className = 'container__card';
+    
+            const cardImage = document.createElement('section');
+            cardImage.className = 'card__image';
+            cardImage.style =`background-image: url('${image}');`;
+
+            const cardValues = document.createElement('ul');
+            cardValues.className = 'card__values';
+
+            const valuesTitle = document.createElement('li');
+            valuesTitle.className = 'values__title';
+            valuesTitle.innerText = title;
+            cardValues.appendChild(valuesTitle);
+
+            const valuesPrice = document.createElement('li');
+            valuesPrice.className = 'values__price';
+            valuesPrice.innerText = price;
+            cardValues.appendChild(valuesPrice);
+
+            const valuesDescription = document.createElement('li');
+            valuesDescription.className = 'values__description';
+            valuesDescription.innerText = description;
+            cardValues.appendChild(valuesDescription);
+
+            const valuesCategory = document.createElement('li');
+            valuesCategory.className = 'values__category';
+            valuesCategory.innerText = `Categoria: ${category}`;
+            cardValues.appendChild(valuesCategory);
+
+            const valuesRate = document.createElement('li');
+            valuesRate.className = 'values__rate';
+            const rateStars = document.createElement('div');
+            rateStars.className = 'rate__stars';
+            rateStars.style = `--rating: ${rating.rate};`;
+            rateStars.ariaLabel = `Classificação do produto: ${rating.rate} de 5 estrelas.`;
+            valuesRate.appendChild(rateStars);
+            cardValues.appendChild(valuesRate);
+
+            containerCard.appendChild(cardImage);
+            containerCard.appendChild(cardValues);
+            container.appendChild(containerCard);
+        }
+
+
+
+    }
+}
+
+getProducts();
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Fake Store
+
+
+**Uma listagem de produtos utilizando a API fakestore.**
+**Link da API: [https://fakestoreapi.com/docs](https://fakestoreapi.com/docs)**
+
+## Tecnologias Utilizadas
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) ![Javascript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+
+
+**Link da loja: [https://jonas-holanda.github.io/fake-store](https://jonas-holanda.github.io/fake-store)**
+
+
+<p align="center">
+    <strong>Copyright © <a href="https://github.com/jonas-holanda" target="_blank">Jonas Holanda</a>. All Rights Reserved.</strong>
+</p>
